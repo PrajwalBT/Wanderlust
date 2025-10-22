61bc70f4584db69fca79a4547908d663efbb022e
Prajwal BT <prajwalbtpraju@gmail.com>
2025-10-23 01:33:41 +0530

    implemented router.route

--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,20 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware");
 
-const userController = require("../controllers/users")
+const userController = require("../controllers/users");
 
-router.get("/signup", userController.renderSignupForm);
+router.route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-router.post("/signup", wrapAsync(userController.signup));
-
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login",
-  saveRedirectUrl,
+router.route("/login")
+  .get(userController.renderLoginForm)
+  .post(saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login", failureFlash: true
   }),
   userController.login
-);
+  );
 
 router.get("/logout", userController.logout);
 
