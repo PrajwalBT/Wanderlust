b8539132fe5ade4eaeab214b159fae4478f27503
Prajwal BT <prajwalbtpraju@gmail.com>
2025-10-22 12:41:50 +0530

    basic signup and login

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouer = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -51,15 +55,33 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));   //for Logion and SignUp 
+
+passport.serializeUser(User.serializeUser());     //storing User info. in session
+passport.deserializeUser(User.deserializeUser()); //unstoring User info. from session
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
+// app.get("/demoUSer", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "prajwalbt"
+//   });
+
+//   let registeredUser = await User.register(fakeUser, "helloworld");  
+//   console.log(registeredUser);
+// });
+
 // Restructuring using Express Router
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouer);
+app.use("/", userRouter);
 
 // Error Handling
 app.all("/*splat", (req, res, next) => {
