bbaff18b2f40ec2a3b2a839564ad5f35b970bb47
Prajwal BT <prajwalbtpraju@gmail.com>
2025-10-26 21:51:06 +0530

    image preview for Edit Page

--- controllers/listings.js
+++ controllers/listings.js
@@ -40,7 +40,10 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash("error", "Listing you requested for does not exist!");
     return res.redirect("/listings");
   }
-  res.render("listing/edit.ejs", { listing });
+
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+  res.render("listing/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
